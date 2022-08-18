fb6495a5bcb4021fba980f63f0b02a97f7a58f8c
Tristen Everett <spock214@gmail.com>
2022-08-18 15:43:31 -0700

    connect to firebase

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import surveyNavReducer from "./survey-nav-reducer";
 import surveySelectReducer from "./survey-select-reducer";
+import { firestoreReducer } from "redux-firestore";
 
 const rootReducer = combineReducers({
   surveyNav: surveyNavReducer,
-  surveySelect: surveySelectReducer
+  surveySelect: surveySelectReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
