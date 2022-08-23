d98587afd87fcac0cfe4b0d7bf5a0bf643aba734
Tristen Everett <spock214@gmail.com>
2022-08-23 14:10:45 -0700

    add ability to sign in / log in / log out with a redirect on successful actions

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,55 +1,116 @@
 import React from "react";
+import firebase from "firebase/app";
+import { withFirestore, isLoaded } from "react-redux-firebase";
+import { Redirect } from "react-router-dom";
 
-function Account() {
-  //Temp values to control routing
-  const loggedIn = false;
-
-  //Return Logic
-  if(loggedIn === true){
-    return (
-      <React.Fragment>
-        <h1>Sign Out</h1>
-        <button>Sign out</button>
-      </React.Fragment>
-    );
+class Account extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      redirectTo: null
+    }
   }
-  if(loggedIn === false){
-    return (
-      <React.Fragment>
-        <h1>Sign In</h1>
-        <form>
-          <input
-            type="text"
-            name="signinEmail"
-            placeholder="email" />
-          <input
-            type="password"
-            name="signinPassword"
-            placeholder="Password" />
-          <button type="submit">Sign in</button>
-        </form>
-  
-        <br />
-        <br />
-        <h1>OR</h1>
-        <br />
-        <br />
 
-        <h1>Sign up</h1>
-        <form>
-          <input
-            type="text"
-            name="email"
-            placeholder="email" />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password" />
-          <button type="submit">Sign up</button>
-        </form>
-      </React.Fragment>
-    );
+  //Internal Functions
+  redirectFunction = (consoleMessage, location = null) => {
+    console.log(consoleMessage);
+    this.setState({
+      redirectTo: location
+    });
+  }
+
+  //Auth Functions
+  DoSignUp = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(this.redirectFunction("Successfully signed up", "/dashboard"))
+      .catch(function(error){
+        console.log(error.message);
+      });
+  }
+
+  DoSignIn = (event) => {
+    event.preventDefault();
+    const email = event.target.signInEmail.value;
+    const password = event.target.signInPassword.value;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.redirectFunction("Successfully signed in", "/dashboard"))
+      .catch(function(error){
+        console.log(error.message);
+      });
+  }
+
+  DoSignOut = () => {
+    firebase.auth().signOut()
+      .then(this.redirectFunction("Successfully signed out", "/"))
+      .catch(function(error){
+        console.log(error.message);
+      });
+  }
+
+  render(){
+    const auth = this.props.firebase.auth();
+    
+    if(this.state.redirectTo !== null){
+      return(
+        <Redirect to={this.state.redirectTo} />
+      )
+    }
+    if(!isLoaded(auth)){
+      return(
+        <React.Fragment>
+          <h1>Loading...</h1>
+        </React.Fragment>
+      );
+    }
+    if (isLoaded(auth) && auth.currentUser != null) {
+      return (
+        <React.Fragment>
+          <h1>Sign Out</h1>
+          <button onClick={this.DoSignOut}>Sign out</button>
+        </React.Fragment>
+      );
+    }
+    if(isLoaded(auth) && auth.currentUser == null){
+      return (
+        <React.Fragment>
+          <h1>Sign In</h1>
+          <form onSubmit={this.DoSignIn}>
+            <input
+              type="text"
+              name="signInEmail"
+              placeholder="email" />
+            <input
+              type="password"
+              name="signInPassword"
+              placeholder="Password" />
+            <button type="submit">Sign in</button>
+          </form>
+    
+          <br />
+          <br />
+          <h1>OR</h1>
+          <br />
+          <br />
+  
+          <h1>Sign up</h1>
+          <form onSubmit={this.DoSignUp}>
+            <input
+              type="text"
+              name="email"
+              placeholder="email" />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password" />
+            <button type="submit">Sign up</button>
+          </form>
+        </React.Fragment>
+      );
+    }
   }
 }
 
-export default Account;
+export default withFirestore(Account);
--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ import App from './components/App';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 import { createFirestoreInstance } from 'redux-firestore';
 import firebase from './firebase';
+import "firebase/auth";
 
 const store = createStore(rootReducer);
 
 const rrfProps = {
   firebase,
   config: {
-    userProfile: "users"
+    userProfile: "users",
+    useFirestoreForProfile: true
   },
   dispatch: store.dispatch,
   createFirestoreInstance
