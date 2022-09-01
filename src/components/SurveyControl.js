bcc591fbf9f4549c31e05ca21407dfcb3bab1aa3
Tristen Everett <spock214@gmail.com>
2022-09-01 13:39:09 -0700

    remove temp data and connect routing logic to user authentication

--- src/components/SurveyControl.js
+++ src/components/SurveyControl.js
@@ -5,71 +5,18 @@ import SurveyList from "./SurveyList";
 import SurveyAnswerForm from "./SurveyAnswerForm";
 import SurveyResults from "./SurveyResult";
 import { useSelector } from "react-redux";
+import firebase from "firebase";
 
-function SurveyControl(props){
-  //Temp prop values
-  const result1 = {
-    id: "q1",
-    surveyId: "1a",
-    response1: "4 Star",
-    response2: "1 Star",
-    response3: "none",
-    response4: "5 Star",
-    response5: "Moonstruck, Reese's, Hershey"
-  };
-  const result2 = {
-    id: "w2",
-    surveyId: "2b",
-    response1: "1 Star",
-    response2: "1 Star",
-    response3: "depends on the seasoning",
-    response4: "1 Star",
-    response5: "salads are garbage"
-  };
-  const result3 = {
-    id: "e3",
-    surveyId: "1a",
-    response1: "2 Star",
-    response2: "5 Star",
-    response3: "mango, guava, pineapple",
-    response4: "3 Star",
-    response5: "Starburst, Skittles, Mike & Ikes"
-  };
-  const result4 = {
-    id: "r4",
-    surveyId: "3c",
-    response1: "4 Star",
-    response2: "1 Star",
-    response3: "I am pescatarian",
-    response4: "2 Star",
-    response5: "shrimp, scallops, cod"
-  };
-  const result5 = {
-    id: "t5",
-    surveyId: "3c",
-    response1: "5 Star",
-    response2: "5 Star",
-    response3: "T-bone, sirloin, ribs",
-    response4: "1 Star",
-    response5: "beef is all I need"
-  };
-  const result6 = {
-    id: "y6",
-    surveyId: "2b",
-    response1: "5 Star",
-    response2: "4 Star",
-    response3: "pickles, tomatoes, broccoli",
-    response4: "2 Star",
-    response5: "I only eat cesar salads"
-  };
-  const resultList = [result1, result2, result3, result4, result5, result6];
-  
-  //Temp values to control routing
-  const correctUserBool = true;
-
-  //Redux Values
+function SurveyControl(){
+  //Value Logic
   const surveyNav = useSelector((state) => state.surveyNav);
   const surveySelect = useSelector((state) => state.surveySelect);
+  const currentUser = firebase.auth().currentUser;
+  let correctUserBool = false;
+  
+  if(surveySelect != null){
+    correctUserBool = (currentUser.uid === surveySelect.creator);
+  }
 
   //Routing Logic
   let currentComponent = null;
@@ -79,18 +26,19 @@ function SurveyControl(props){
     currentComponent = <SurveyList />;
   }else if(surveyNav.edit === true){
     if(correctUserBool === true){
-      currentComponent = <SurveyEdit survey={surveySelect} />;
+      currentComponent = <SurveyEdit />;
     }else{
       currentComponent = <h2>Please log in to edit</h2>;
     }
   }else{
     if(correctUserBool === true){
-      currentComponent = <SurveyResults responses={resultList} survey={surveySelect} />;
+      currentComponent = <SurveyResults />;
     }else{
-      currentComponent = <SurveyAnswerForm survey={surveySelect} />;
+      currentComponent = <SurveyAnswerForm />;
     }
   }
 
+  //Return Logic
   return (
     <React.Fragment>
       {currentComponent}
